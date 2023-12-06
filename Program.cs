afe0018d8a24dfa9e6c49b53d7fff18476db9c3a
A252557 <A252557@LSBSA-3WCCHW3>
2023-12-06 14:43:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.if_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            if (isMale)
+            {
+                Console.WriteLine("you are male");
+            }
+            else
+            {
+                Console.WriteLine("you are female");
+            }
+            bool isTall = false;
+            if (isTall && isMale) {
+                Console.WriteLine("You are both male and tall.");
+            }
+            else if (!isTall && isMale) {
+                Console.WriteLine("You are not tall but you are male.");
+            
+            }
+            else { Console.WriteLine("you are either not tall or not a male."); }
+        }
+    }
+}
